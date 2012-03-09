c93cab17bae8e709cf9cf4d5a3242be3f500a3c4
Olivier Larchevêque <olivier.larcheveque@gmail.com>
2012-03-09 00:16:18 -0500

    cleanup

--- usin/skeletons/templates/django_buildout/project/development.py
+++
@@ -1,5 +0,0 @@
-
-from project.settings import *
-DEBUG=True
-TEMPLATE_DEBUG=DEBUG
-HTTPS_SUPPORT=False
--- usin/skeletons/templates/django_buildout/project/finders.py
+++
@@ -1,8 +0,0 @@
-from staticfiles.finders import AppDirectoriesFinder
-from staticfiles.storage import AppStaticStorage
-
-class LegacyAppStaticStorage(AppStaticStorage):
-    source_dir = 'media'
-
-class AppMediaFinder(AppDirectoriesFinder):
-    storage_class = LegacyAppStaticStorage
