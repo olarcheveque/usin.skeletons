aa45ded25fb0c1933ef582482ea9fd2d46e173a6
Olivier Larchevêque <olivier.larcheveque@gmail.com>
2012-03-09 01:17:58 -0500

    1.0

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+version = '1.0'
+name = 'usin.skeletons'
+setup(name=name,
+      version=version,
+      description="Skeletons used at Usinasite",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='skeleton paster django usinasite',
+      author='Olivier Larchev\xc3\xaaque',
+      author_email='olivier.larcheveque@gmail.com',
+      url='http://pypi.usinasite.com/%s' % name,
+      license='GPL',
+      namespace_packages = ['usin', 'usin.skeletons', ],
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          'PasteScript >= 1.7.4.2',
+          'Cheetah >= 2.4.4',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [paste.paster_create_template]
+      usin_django = usin.skeletons.django:DjangoBuildout
+      """,
+      )
---
+++ usin/skeletons/django.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+
+import os
+from paste.script import templates
+
+
+class DjangoBuildout(templates.Template):
+
+    special_tpl_files = {
+        'gitignore': '.gitignore',
+    }
+
+    summary = u'Skeleton to start Django project with Buildout'
+    required_templates = []
+    _template_dir = 'templates/django_buildout'
+    use_cheetah = True
+
+    vars = (
+    )
+
+    def rename_files(self, output_dir):
+        for f_tpl, f_real in self.special_tpl_files.items():
+            f_tpl = os.path.join(output_dir, f_tpl)
+            f_real = os.path.join(output_dir, f_real)
+            os.rename(f_tpl, f_real)
+
+    def post(self, command, output_dir, vars):
+        self.rename_files(output_dir)
